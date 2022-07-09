1bbf3ae65bfe4cf1e50314185f61d8122539ba51
lovinmaxwell <lovinmaxwell@gmail.com>
2022-07-09 19:19:27 +0000

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Be You Iris
+
+Be You Iris Qatar App
+
+#### License
+
+MIT
---
+++ beyouiris/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Be You Iris",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Be You Iris")
+		}
+	]
